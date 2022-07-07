4f99c38494667a4274171894e701f73ce32edaff
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-07-07 12:04:59 +0100

    adding apiFeatures

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -7,9 +7,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
-// }
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
