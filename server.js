62f6e516992350ac4c7c6b24153156a7adce8b37
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-07-07 11:59:06 +0100

    adding apiFeatures

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ const DB = process.env.DATABASE;
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful'))
