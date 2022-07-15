7befa74e8744640388c535d808eac290821bba3b
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-07-15 12:48:45 +0100

    adding header and footer templates

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
+// Start express app
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // MIDDLEWARES
 // Development logging
@@ -14,16 +23,22 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+// Test middleware
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
+  console.log('Hello from the middelware');
+  // console.log(req.cookies);
   next();
 });
 
 // ROUTES
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
