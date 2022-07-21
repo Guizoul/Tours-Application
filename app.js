682fad40fd3f5157ff8f2a6d8296ab5009fa2a1d
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-07-21 16:34:00 +0100

    fixing app

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // MIDDLEWARES
 // Development logging
 if (process.env.NODE_ENV === 'development') {
