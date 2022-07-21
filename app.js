5c4ef506c6bdb64fbb87f7e896b45d5378bfe710
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-07-21 16:18:38 +0100

    fixing app

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
